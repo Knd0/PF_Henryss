b0a486deb782663114b6caec3432ac964760793d
Franco D <luafranco09@hotmail.com>
2023-02-28 11:57:56 -0300

    Filtros terminados (A revisar y probar), se puede agregar algun otro

---
+++ client/Components/Filters/Filters.jsx
@@ -0,0 +1,92 @@
+import { React, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCars, filterByBrand, filterByYear, orderByAlf, orderByKM, orderByPrice, cleanState } from "../../redux/actions";
+
+export default function Filters() {
+    const dispatch = useDispatch();
+    const cars = useSelector((state) => state.cars);
+    const [orden, setOrden] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        dispatch(getCars());
+        dispatch(cleanState());
+    }, [dispatch]);
+
+    function handleFilteredBrand(e) {
+        dispatch(filterByBrand(e.target.value));
+    }
+
+    function handleFilteredYear(e) {
+        dispatch(filterByYear(e.target.value));
+    }
+
+    function handleSortAlf(e) {
+        e.preventDefault();
+        dispatch(orderByAlf(e.target.value));
+        setCurrentPage(1);
+        setOrden(`Ordenado ${e.target.value}`);
+    }
+
+    function handleSortKM(e) {
+        e.preventDefault();
+        dispatch(orderByKM(e.target.value));
+        setCurrentPage(1);
+        setOrden(`Ordenado ${e.target.value}`);
+    }
+
+    function handleSortPrice(e) {
+        e.preventDefault();
+        dispatch(orderByPrice(e.target.value));
+        setCurrentPage(1);
+        setOrden(`Ordenado ${e.target.value}`);
+    }
+
+    return (
+    <div className={style.filtros}>
+        <div>
+            Order alphabetically:
+            <select className={style.select} onChange={(e) => handleSortAlf(e)}>
+                <option value="all">All</option>
+                <option value="asc">A - Z</option>
+                <option value="desc">Z - A</option>
+            </select>
+        </div>
+        <div>
+            Order by price:
+            <select className={style.select} onChange={(e) => handleSortPrice(e)}>
+                <option value="all">All</option>
+                <option value="mayp">Minor to Major</option>
+                <option value="menp">Major to Minor</option>
+            </select>
+        </div>
+        <div>
+            Order by KM:
+            <select className={style.select} onChange={(e) => handleSortKM(e)}>
+                <option value="all">All</option>
+                <option value="mayp">Minor to Major</option>
+                <option value="menp">Major to Minor</option>
+            </select>
+        </div>
+        <div>
+            Filter by year:
+            <select className={style.select} onChange={e => handleFilteredYear(e)}>
+                <option value="all">All</option>
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+            </select>
+        </div>
+        <div>
+            Filter by brand:
+            <select className={style.select} onChange={e => handleFilteredBrand(e)}>
+                <option></option>                      
+                {cars.map((car) => (
+                <option>{car.brand}</option>
+                ))}
+            </select>
+        </div>
+    </div>
+    )
+}
