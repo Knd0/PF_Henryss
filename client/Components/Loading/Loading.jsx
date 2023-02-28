cdd231b9cbdbaee701b919640002f829a951d5ed
SantiSilvera02 <sshojita11@gmail.com>
2023-02-28 10:54:56 -0300

    agregado de carpeta componente/NavBar en proceso

---
+++ client/Components/Loading/Loading.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div className="loadingcontainer">
+      <div className="contentloading">
+        <h1 className="cargando">CARGANDO</h1>
+        <span className="loading"></span>
+      </div>
+    </div>
+  );
+}
+// importar la funcion donde exista un tiempo de carga o de esperea (CarsDetails.jsx , Home.jsx, NavBar.jsx , etc,)
+//import Loading from ../Loading/Loading";
+//const loading = useSelector((state) => state.loading);
+//if (loading === true)
+
+//// MODIFICACION de reducer.js
+// case "LOADING_ACTION":
+//     const loading = state.loading;
+//     if (loading === true) {
+//       return {
+//         ...state,
+//         loading: false,
+//       };
+//     } else {
+//       return {
+//         ...state,
+//         loading: true,
+//       };
+//     }
