564b9928ec237761c768b425def1d731b90d3fee
David <davidurquijoh@gmail.com>
2023-02-28 15:39:23 -0500

    Controllers

--- api/src/handlers/carsHandlers.js
+++ api/src/handlers/carsHandlers.js
@@ -1,7 +1,25 @@
+const { getApiCars, getCarByMake, getCarByModel } = require("../controllers/carsControllers")
 
 
 const getCars = (req, res) => {
-    res.send('estoy en la ruta getCars')
+    const { make } = req.query
+    const { model } = req.query
+    try {
+        if(make){
+            const response = getCarByMake(make);
+            res.status(200).send(response) 
+        }
+        if(model){
+            const response = getCarByModel(model);
+            res.status(200).send(response) 
+        }
+        else {
+            const response = getApiCars();
+            res.status(200).send(response)
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 }
 
 
