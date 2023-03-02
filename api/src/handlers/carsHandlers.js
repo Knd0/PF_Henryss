fd48ccb4d7223787be09622b0012ddf762ff7358
Isidro Rivera <dca49@hotmail.com>
2023-03-01 18:22:00 -0600

    fix

--- api/src/handlers/carsHandlers.js
+++ api/src/handlers/carsHandlers.js
@@ -2,11 +2,11 @@ const { getApiCars, getCarByBrand, getCarByModel, getCarDetail } = require("../c
 
 
 const getCars = (req, res) => {
-    const { brand } = req.query
-    const { model } = req.query
+    var { name } = req.query
+    var{ name } = req.query
     try {
-        if(brand){
-            const response = getCarByBrand(brand);
+        if(name){
+            const response = getCarByBrand(name);
             res.status(200).send(response) 
         } 
         else if(model){
