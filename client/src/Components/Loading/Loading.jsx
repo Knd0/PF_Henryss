d686f827c6eca9e541b07fd14b9313ef641b9b57
SantiSilvera02 <sshojita11@gmail.com>
2023-03-02 15:39:30 -0300

    Loading ruedita

---
+++ client/src/Components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div class="container">
+        <div 
+            class="spinner">
+
+        </div>
+  </div>
+  )
+}
+
