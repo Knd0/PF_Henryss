5f8308aa25259ab449e64a6c1944864c6ffb285b
Franco D <luafranco09@hotmail.com>
2023-03-03 10:11:34 -0300

    Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../src/App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux"
-import {persistor,store} from '../src/Redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "../src/App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Provider } from "react-redux";
+import { persistor, store } from "../src/Redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-     <PersistGate persistor={persistor}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate persistor={persistor}>
+      <Auth0Provider
+        domain="francodeiriondo.us.auth0.com"
+        clientId="a6MGlYll7XRzZvH2Z3LNuNp5cCkMQe0Z"
+        redirectUri={window.location.origin}
+      >
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Auth0Provider>
     </PersistGate>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
