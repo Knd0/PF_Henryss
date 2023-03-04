3d3074c7476984d41022ddfb6be1604d4854cc94
David <davidurquijoh@gmail.com>
2023-03-04 08:40:32 -0500

    Correccion de bug User

---
+++ api/src/models/User.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+    sequelize.define('User', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        require: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+    },
+    // image: {
+    //     type: DataTypes.ARRAY,
+    // },
+    admin: {
+        type: DataTypes.BOOLEAN,
+        default: false,
+},
+active: {
+        type: DataTypes.BOOLEAN,
+        default: true,
+},
+
+    });
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const brandRoutes = require('./brandRoutes');
 const carsRoutes = require('./carsRoutes')
 const favoritesRoutes = require('./favoritesRoutes')
+const usersRoutes = require('./usersRoutes')
 
 const router = Router();
 
 router.use('/cars', carsRoutes);
 router.use('/brands', brandRoutes)
 router.use('/favorites', favoritesRoutes)
+router.use('/user', usersRoutes)
 
 module.exports = router;
