9c99bb3daeb0f6f4062d569ecb9ec1eb4b6e4d3a
Franco D <luafranco09@hotmail.com>
2023-03-06 19:42:48 -0300

    a

--- client/src/Components/Login/Logout.jsx
+++ client/src/Components/Login/Logout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch } from "react-redux";
-import styles from "./Logout.module.css";
 
 export const LogoutButton = () => {
   const { logout } = useAuth0();
@@ -9,7 +8,6 @@ export const LogoutButton = () => {
 
   return (
     <button
-      className={styles.button}
       type="button"
       onClick={() => {
         dispatch({ type: "GET_USER_PROFILE", payload: "" });
