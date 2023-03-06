58665477e3dca2c879b6346e841f7f80078e5f65
LucasIriarte <lucasiriarte67@gmail.com>
2023-03-06 20:18:22 -0300

    Home responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,108 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import img from "../Img/Home.png";
-import style from "../Navbar/Navbar.module.css";
 
 export default function Navbar() {
+  const [barsDropDownMenu, setBarsDropDownMenu] = useState(false)
+  const handleBarsDropDownMenu = () => {
+    console.log("Hola")
+    barsDropDownMenu ? setBarsDropDownMenu(false) : setBarsDropDownMenu(true)
+  }
+  const [actualPage, setActualPage] = useState({})
+  const handleCurrentPage = (e) => {
+    setActualPage({
+      [e.target.name]:e.target.name
+    })
+    console.log(actualPage)
+  }
   return (
     <>
-      <nav class="relative mb-30 bg-white px-2 sm:px-4 py-5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-        <div class="container flex flex-wrap items-center justify-between mx-auto">
-          <Link to="/">
-            <img
-              className={style.img}
-              src={img}
-              class="h-20 mr-10 sm:h-20"
-              alt="Flowbite Logo"
-            />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Radiator Springs
-            </span>
-          </Link>
-          <div class="flex md:order-2">
-            <button
-              type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Login
-            </button>
-            <button
-              data-collapse-toggle="navbar-sticky"
-              type="button"
-              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-sticky"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Open main menu</span>
-              <svg
-                class="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-sticky"
-          >
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <Link
-                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                  aria-current="page"
-                  to="/"
-                >
-                  Home
-                </Link>
-              </li>
 
-              <li>
-                <Link
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  to="/cars"
-                >
-                  Cars
-                </Link>
-              </li>
+      {/* ------------------------------FUNCIONANDO---------------------------- */}
 
-              <li>
-                <Link
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  to="/aboutus"
-                >
-                  About Us
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  to="/myfavorites"
-                >
-                  My Favorites
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  to="/carscreate"
-                >
-                  Create your own Publish
-                </Link>
-              </li>
-            </ul>
+      {/* <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <img src={img} alt="logo" className={style.img} />
+        </div>
+        <div class="block lg:hidden">
+          <button onClick={handleBarsDropDownMenu} id='boton' class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          </button>
+        </div>
+        <div id='menu' class={barsDropDownMenu ? "w-full block flex-grow lg:flex lg:items-center lg:w-auto" : "w-full block flex-grow lg:flex lg:items-center lg:w-auto max-lg:hidden"}>
+          <div class="text-sm lg:flex-grow">
+            <Link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Home
+            </Link>
+            <Link to="/cars" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              About Us
+            </Link>
+            <Link to="/aboutus" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              My Favorites
+            </Link>
+            <Link to="/carscreate" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Create your own Publish
+            </Link>
           </div>
         </div>
+      </nav> */}
+
+      {/* ------------------------------FUNCIONANDO---------------------------- */}
+
+      <nav class="flex items-center justify-between flex-wrap bg-white">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <img src={img} alt="logo" class="w-20 mt-3 ml-3" />
+        </div>
+        <div class="block sm:hidden">
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
+          <button onClick={handleBarsDropDownMenu} id='boton' class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <svg
+              class="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div id='menu' class={barsDropDownMenu ? "w-full block flex-grow sm:flex sm:items-center sm:w-auto" : "w-full block flex-grow sm:flex sm:items-center sm:w-auto max-lg:hidden"}>
+          <div class="text-sm sm:flex-grow">
+            <Link to="/" class="font-bold block mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:underline mr-4" name="home" onClick={handleCurrentPage}>
+              Home
+            </Link>
+            <Link to="/cars" class="font-bold block mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:underline mr-4" name="cars" onClick={handleCurrentPage}>
+              Cars
+            </Link>
+            <Link to="/aboutus" class="font-bold block mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:underline mr-4" name="aboutUs" onClick={handleCurrentPage}>
+            About Us
+            </Link>
+            <Link to="/myfavorites" class="font-bold block mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:underline mr-4" name="myFavorites" onClick={handleCurrentPage}>
+              My Favorites
+            </Link>
+            <Link to="/carscreate" class="font-bold block mt-4 sm:inline-block sm:mt-0 text-gray-700 hover:underline mr-4" name="createYourOwnPublish" onClick={handleCurrentPage}>
+              Create your own Publish
+            </Link>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          class="max-sm:hidden my-auto self-end text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Login
+        </button>
       </nav>
+
+
+
     </>
   );
 }
