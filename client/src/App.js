a971fab5b4ad43afefc8fa825dfc694b72063cfd
David <davidurquijoh@gmail.com>
2023-03-09 20:34:03 -0500

    Borrado de app.js la ruta localHost

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ import CarsCreate from "./Components/CarFormPage/CarsCreate";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import axios from "axios";
 axios.defaults.baseURL = 'https://pfhenryss-production.up.railway.app/'
-// axios.defaults.baseURL = "http://localhost:3001/"; // Cuando vayan a probar en Local comentan la linea de arriba y descomentan esta
 
 function App() {
   return (
