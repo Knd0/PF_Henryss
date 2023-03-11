dc8063900f3650518f29321b5a11b447b011c768
LucasIriarte <lucasiriarte67@gmail.com>
2023-03-10 21:59:31 -0300

    renderización de cards favoritas

--- client/src/Components/MyFavorites/MyFavorites.jsx
+++ client/src/Components/MyFavorites/MyFavorites.jsx
@@ -37,34 +37,49 @@ export default function MyFavorites() {
     dispatch(getCars())
     dispatch(getUsersDetails(user.email))
     dispatch(getFavorites(userDetails[0].userId))
-  }, [dispatch])
+  }, [dispatch, user.email, userDetails])
 
 
+  if (!favorites.length) {
+    return (<>
+      <Navbar />
+      <h1>My favorites</h1>
+      <div className={style.cardconteinerFavorite}>
+        <div className={style.cardModal}>
+          <h3>Nothing to show 🤔</h3>
+        </div>
+      </div>
+    </>)
+
+  }
   return (
     <>
       <Navbar />
       <h1>My Favorites</h1>
       <div className={style.cardconteinerFavorite}>
         {
-          !favorites.length ? (
-            <div className={style.cardModal}>
-              <h3>Nothing to show 🤔</h3>
-            </div>
-          ): 
-          favorites.map(e =>
-            <Card
-              carId={e[0].carId}
-              brand={e[0].brand}
-              img={e[0].img.secure_url}
-              model={e[0].model}
-              year={e[0].year}
-              kilometers={e[0].kilometers}
-              price={e[0].price}
-            />) 
+          favorites.length ?
+            favorites.map(e =>
+              <Card
+                carId={e[0].carId}
+                brand={e[0].brand}
+                img={e[0].img.secure_url}
+                model={e[0].model}
+                year={e[0].year}
+                kilometers={e[0].kilometers}
+                price={e[0].price}
+              />)
+            :
+            (
+              <div className={style.cardModal}>
+                <h3>Nothing to show 🤔</h3>
+              </div>
+            )
         }
       </div>
       <div></div>
-      <div><Pagination pagina={currentPage} setPagina={setCurrentPage} maximo={maximo} /></div>
+      <div>
+        <Pagination pagina={currentPage} setPagina={setCurrentPage} maximo={maximo} /></div>
       <Footer />
     </>
 
