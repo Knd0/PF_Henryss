4ee2d72bf88b1bb58cec9901d36291dbe2bbf856
LucasIriarte <lucasiriarte67@gmail.com>
2023-03-10 22:20:46 -0300

    Cards en favoritos

--- client/src/Components/MyFavorites/MyFavorites.jsx
+++ client/src/Components/MyFavorites/MyFavorites.jsx
@@ -17,6 +17,9 @@ import Loading from "../Loading/Loading";
 
 export default function MyFavorites() {
   const dispatch = useDispatch()
+  const favorites = useSelector((state) => state.favorites)
+  const { user } = useAuth0()
+  const userDetails = useSelector((state) => state.usersDetails)
   const [currentPage, setCurrentPage] = useState(1)
   const [carsPerPage, setCountriesPerPage] = useState(8)
   const allcars = useSelector((state) => state.allcars)
@@ -27,61 +30,100 @@ export default function MyFavorites() {
   const cars = useSelector((state) => state.allcars);
   const loading = useSelector((state) => state.loading);
   const [input, setInput] = useState("");
-  const favorites = useSelector((state) => state.favorites)
   const maximo = favorites.length / carsPerPage
-  const { user } = useAuth0()
-  const userDetails = useSelector((state) => state.usersDetails)
 
   useEffect(() => {
     dispatch(cleanState());
     dispatch(getCars())
     dispatch(getUsersDetails(user.email))
     dispatch(getFavorites(userDetails[0].userId))
-  }, [dispatch, user.email, userDetails])
+  }, [])
 
 
-  if (!favorites.length) {
+  if(!userDetails[0].favorites.length) {
     return (<>
-      <Navbar />
-      <h1>My favorites</h1>
-      <div className={style.cardconteinerFavorite}>
-        <div className={style.cardModal}>
-          <h3>Nothing to show 🤔</h3>
-        </div>
-      </div>
-    </>)
-
+         <Navbar />
+         <h1>My favorites</h1>
+         <div className={style.cardconteinerFavorite}>
+           <div className={style.cardModal}>
+             <h3>Nothing to show 🤔</h3>
+           </div>
+         </div>
+       </>)
   }
   return (
-    <>
-      <Navbar />
-      <h1>My Favorites</h1>
-      <div className={style.cardconteinerFavorite}>
-        {
-          favorites.length ?
-            favorites.map(e =>
-              <Card
-                carId={e[0].carId}
-                brand={e[0].brand}
-                img={e[0].img.secure_url}
-                model={e[0].model}
-                year={e[0].year}
-                kilometers={e[0].kilometers}
-                price={e[0].price}
-              />)
-            :
-            (
-              <div className={style.cardModal}>
-                <h3>Nothing to show 🤔</h3>
-              </div>
-            )
-        }
-      </div>
-      <div></div>
-      <div>
-        <Pagination pagina={currentPage} setPagina={setCurrentPage} maximo={maximo} /></div>
-      <Footer />
-    </>
-
+      <>
+        <Navbar />
+        <h1>My Favorites</h1>
+        <div className={style.cardconteinerFavorite}>
+          {
+            favorites.length ?
+              favorites.map(e =>
+                <Card
+                  carId={e[0].carId}
+                  brand={e[0].brand}
+                  img={e[0].img.secure_url}
+                  model={e[0].model}
+                  year={e[0].year}
+                  kilometers={e[0].kilometers}
+                  price={e[0].price}
+                />)
+              :
+              (
+                <div className={style.cardModal}>
+                  <h3>Nothing to show 🤔</h3>
+                </div>
+              )
+          }
+        </div>
+        <div></div>
+        <div>
+          <Pagination pagina={currentPage} setPagina={setCurrentPage} maximo={maximo} /></div>
+        <Footer />
+      </>
   )
+  // if (!favorites.length) {
+  //   return (<>
+  //     <Navbar />
+  //     <h1>My favorites</h1>
+  //     <div className={style.cardconteinerFavorite}>
+  //       <div className={style.cardModal}>
+  //         <h3>Nothing to show 🤔</h3>
+  //       </div>
+  //     </div>
+  //   </>)
+
+  // }
+  // return (
+  //   <>
+  //     <Navbar />
+  //     <h1>My Favorites</h1>
+  //     <div className={style.cardconteinerFavorite}>
+  //       {
+  //         favorites.length ?
+  //           favorites.map(e =>
+  //             <Card
+  //               carId={e[0].carId}
+  //               brand={e[0].brand}
+  //               img={e[0].img.secure_url}
+  //               model={e[0].model}
+  //               year={e[0].year}
+  //               kilometers={e[0].kilometers}
+  //               price={e[0].price}
+  //             />)
+  //           :
+  //           (
+  //             <div className={style.cardModal}>
+  //               <h3>Nothing to show 🤔</h3>
+  //             </div>
+  //           )
+  //       }
+  //     </div>
+  //     <div></div>
+  //     <div>
+  //       <Pagination pagina={currentPage} setPagina={setCurrentPage} maximo={maximo} /></div>
+  //     <Footer />
+  //   </>
+
+  // )
 }
