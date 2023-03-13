b48cddc3b615bf9f19f4c3cf5fff2199b003f734
Franco de Iriondo <91231311+Knd0@users.noreply.github.com>
2023-03-13 16:11:28 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,8 @@ import UserProfile from "./Components/UserProfile/UserProfile";
 import axios from "axios";
 
 import { ProtectedRoute } from "./Components/ProtectedRoute/ProtectedRoute";
-//}}axios.defaults.baseURL = 'https://pfhenryss-production.up.railway.app/'
- axios.defaults.baseURL = "http://localhost:3001/"; // Cuando vayan a probar en Local comentan la linea de arriba y descomentan esta
+axios.defaults.baseURL = 'https://pfhenryss-production.up.railway.app/
+// axios.defaults.baseURL = "http://localhost:3001/"; // Cuando vayan a probar en Local comentan la linea de arriba y descomentan esta
 
 
 function App() {
