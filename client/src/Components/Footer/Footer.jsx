b89047dffe4c88f7ed6b9258f00ef923fcf3b820
Franco D <luafranco09@hotmail.com>
2023-03-15 15:36:56 -0300

    fix

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -86,7 +86,7 @@ export default function Footer() {
             </a>
           </div>
           <p className="mt-8 text-base leading-6 text-center text-gray-400">
-            © 2023 RadiadorSprings, Inc. All rights reserved.
+            © 2023 Radiator Springs, Inc. All rights reserved.
           </p>
         </div>
       </section>
