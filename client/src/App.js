b4112ec853372b69d572315d7e15b673d54b6677
Isidro Rivera <dca49@hotmail.com>
2023-03-15 21:10:04 -0600

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,8 @@ import UpdateReview from "./Components/Reviews/UpdateReview";
 import axios from "axios";
 
 import { ProtectedRoute } from "./Components/ProtectedRoute/ProtectedRoute";
-axios.defaults.baseURL = "https://pfhenryss-production.up.railway.app/"
-// axios.defaults.baseURL = "http://localhost:3001/"; // Cuando vayan a probar en Local comentan la linea de arriba y descomentan esta
+//axios.defaults.baseURL = "https://pfhenryss-production.up.railway.app/"
+ axios.defaults.baseURL = "http://localhost:3001/"; // Cuando vayan a probar en Local comentan la linea de arriba y descomentan esta
 
 
 
