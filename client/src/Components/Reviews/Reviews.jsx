db5ee32153e11c76e9463dad20468209954a4af9
dayanna <vidaurredayanna@gmail.com>
2023-03-21 21:09:18 -0500

    estilos de review

--- client/src/Components/Reviews/Reviews.jsx
+++ client/src/Components/Reviews/Reviews.jsx
@@ -12,7 +12,7 @@ function Reviews() {
   const reviews = useSelector((state) => state.opinion);
   const fechaActual = new Date();
   const dispatch = useDispatch();
- 
+
   useEffect(() => {
     dispatch(getReviews());
   }, [dispatch]);
@@ -20,7 +20,7 @@ function Reviews() {
   return (
     <>
       <Navbar />
-      <h1>Reviews</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-8">Reviews</h1>
       <Link to={"/starsReviews"}>
         {" "}
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -29,10 +29,25 @@ function Reviews() {
       </Link>
       <div className={styles.contenedor}>
         {reviews?.map((r) => {
-          
           return (
-            <div className={styles.containerItems}>
-              <p>
+            <div className="w-96 h-82 flex flex-col items-start justify-start gap-4 p-4 bg-gray-200 rounded-lg shadow-md ml-8">
+              <div className="mt-8 flex flex-row justify-between w-full text-lg text-gray-400">
+                  <div>
+                        <p>
+                          User:{" "}
+                          <span className="font-medium text-gray-800">{r.name}</span>
+                        </p>
+                  </div>
+                  <div>
+                        <p>
+                          Fecha:{" "}
+                          <span className="font-medium text-gray-800">
+                            {r.updatedAt.slice(0, 10)}
+                          </span>
+                        </p>
+                  </div>
+              </div>
+              <div className="flex items-center gap-2 text-2xl">
                 {[...Array(5)].map((_, index) => (
                   <span
                     key={index}
@@ -44,25 +59,15 @@ function Reviews() {
                     &#9733;
                   </span>
                 ))}
-              </p>
-              <p className="truncate">{r.review}</p>
-              <div className={styles.fechaOpinionContainer}>
-                <div>
-                  <p>
-                    User: <span>{r.name}</span>
-                  </p>
-                </div>
-                <div>
-                  <p>
-                    Fecha: <span>{r.updatedAt.slice(0, 10)}</span>
-                  </p>
-                </div>
+              </div>
+              <div className="bg-white rounded-lg p-4 shadow-md w-full">
+                <p className="w-full h-40 resize-none text-lg">{r.review}</p>
               </div>
             </div>
           );
         })}
       </div>
-      <Footer />
+      <Footer/>
     </>
   );
 }
