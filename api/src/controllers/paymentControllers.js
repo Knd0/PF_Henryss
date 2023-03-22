a618bf8f487352521812ce9d7944eab85593681f
David <davidurquijoh@gmail.com>
2023-03-21 19:13:57 -0500

    Payment email funcional

--- api/src/controllers/paymentControllers.js
+++ api/src/controllers/paymentControllers.js
@@ -1,53 +1,60 @@
-require('dotenv').config();
-const {
-    API_KEY_NODEMAILER,
-    API_KEY_STRIPE
-  } = process.env;
 const Stripe = require('stripe')
-const stripe = new Stripe(API_KEY_STRIPE)
+require('dotenv').config()
+const { API_KEY_STRIPE, email, password } = process.env
+const stripe = new Stripe('sk_test_51Mkjw9ETVvdZ62yxIbqQuiRYkXR7hcFJhuj57N2zmwI998nlKYoLsgRBIEyGqkvNVvVyRsl9lMQHSwIAyfmP0A6L00ew0ZdokN')
 const nodemailer = require("nodemailer");
-const sgTransport = require("nodemailer-sendgrid-transport");
-const { User } = require('../db');
+const { User } = require('../db')
 
-const options = {
-  auth: {
-    api_key: API_KEY_NODEMAILER
-  }
-};
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    secure: false,
+    auth: {
+        user: email,
+        pass: password,
+    },
+});
 
-const transporter = nodemailer.createTransport(sgTransport(options));
+
 
 const pay = async (id, amount, userId) => {
-  if(!userId) return ('You need to login to proceed with the payment');
-  let searchUser = await User.findOne({
-    where: { userId: userId }
-  });
-  if(!searchUser) return ('This user does not exist');
-  const payment = await stripe.paymentIntents.create({
-    amount,
-    currency: "USD",
-    description: "nada",
-    payment_method: id,
-    confirm: true
-  });
+    if(!userId) return ('You need to login to procced with the pay')
+    let searchUser = await User.findOne({
+        where: { userId: userId }
+    });
+    if(!searchUser) return ('This user does not exist')
+    const payment = await stripe.paymentIntents.create({
+        amount,
+        currency: "USD",
+        description: "nada",
+        payment_method: id,
+        confirm: true
 
-  const mailOptions = {
-    from: "66.radiador.springs@gmail.com",
-    to: searchUser.email,
-    subject: "Pago exitoso",
-    text: "Se ha realizado un pago exitoso."
-  };
+    })
+    // Configura el objeto de correo electrónico
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
+    if (payment.status === 'succeeded') {
+        const mailOptions = {
+            from: email,
+            to: searchUser.email,
+            subject: 'Payment Confirmation',
+            text: 'Your payment has been processed successfully!'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        return ('Success payment and email sent');
     } else {
-      console.log("Correo electrónico enviado: " + info.response);
+        return ('Payment failed');
     }
-  });
-  return ('Success payment');
-};
+}
 
 module.exports = {
-  pay
-};
+    pay
+}
